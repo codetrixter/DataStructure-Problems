b03ce34dd43a6fa4250a7185cc0385d90dd87819
codetrixter <abhishek20893@gmail.com>
2019-07-18 08:26:59 +0530

    insertionatend

--- Linked_List/Creation_Of_SLL.cpp
+++ Linked_List/Creation_Of_SLL.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 
 class Node
 {
@@ -22,38 +23,74 @@ void insertAtBeg(Node **head_ref, int element)
     (*head_ref) = newNode;
 }
 
-void printList(Node **head)
+void insertAtEnd(Node **node_head, int element)
 {
-    while((*head)->next != NULL)
+    Node *newNode = new Node();
+
+    newNode->data = element;
+    //adding null to the next of new node.
+    newNode->next = NULL;
+
+    //if node is NULL
+    if((*node_head) == NULL)
     {
-        std::cout << (*head)->data << std::endl;
-        (*head) = (*head)->next;
+        (*node_head) = newNode;
+        return;
+    }
+
+    //traversing the linked list
+    while((*node_head)->next != NULL)
+    {
+        (*node_head) = (*node_head)->next;
+    }
+
+    //insertion at end
+    (*node_head)->next = newNode;
+}
+
+void printList(Node *head)
+{
+    while(head != NULL)
+    {
+        std::cout << head->data << std::endl;
+        head = head->next;
     }
 }
 
 int main(int argc, char const *argv[])
 {
-    int options, element;
+    int options= 0, element = 0;
     Node *head = NULL;
     while(options != 0)
     {
         std::cout << "Please select the following options to perform the respective actions..." << std::endl;
-        std::cout << "1: Insert an Element into the linked liist" << std::endl;
-        std::cout << "2: Print the linked liist" << std::endl;
+        std::cout << "1: Insert an Element into the linked list at the begining" << std::endl;
+        std::cout << "2: Insert an Element into the linked list at the end" << std::endl;
+        std::cout << "3: Insert an Element into the linked list at the given location" << std::endl;
+        std::cout << "4: Print the linked liist" << std::endl;
         std::cout << "0: Exit" << std::endl;
         std::cin >> options;
-        std::cin >> element;
 
         switch(options)
         {
             case 1:
-                std::cout << "input the datta you want to insert at the beginning of the list" << std::endl;
+                std::cout << "input the data you want to insert at the beginning of the list" << std::endl;
+                std::cin >> element;
                 insertAtBeg(&head, element);
                 break;
             case 2:
-                printList(&head);
+                std::cin >> element;
+                insertAtEnd(&head, element);
+                break;
+            case 3:
+            case 4:
+                printList(head);
+                break;
+            case 0:
+                std::cout << "O enetered.... exiting..." << std::endl;
+                std::exit(0);
             default :
-            break;
+                std::cout << "Invalid entry" << std::endl;
         }
     }
     return 0;
