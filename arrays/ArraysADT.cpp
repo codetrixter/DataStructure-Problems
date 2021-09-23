c7e6053fbd076176432f2459aa9ea48d1857d09a
codetrixter <abhishek20893@gmail.com>
2021-09-23 13:03:50 +0530

    updates to arrayADT

--- arrays/ArraysADT.cpp
+++ arrays/ArraysADT.cpp
@@ -1,3 +1,6 @@
+/**
+ * 
+ */
 #include <iostream>
 
 using namespace std;
@@ -34,6 +37,7 @@ public:
      */
     void display()
     {
+        printf("\n");
         for (int i = 0; i < 10; i++)
         {
             printf("%d", A[i]);
@@ -51,19 +55,47 @@ public:
     }
 
     /**
-     * Insert an element into the array at a given index.
+     * Insert an element into the array at a given index. 
+     * Complexity is O(n) in worst case.
      */
-    void insertElement(int index)
+    void insertElement(int element, int index)
     {
+        int i = 0;
+        if(this->getLength() == this->getSize())
+        {
+            printf("array is full... exiting...");
+            return ;
+        }
+            
+        for(i = this->getLength(); i > index; i--)
+        {
+            A[i] = A[i-1];
+        }
 
+        A[i] = element;
+        this->length++;
     }
 
     /**
      * Deletes from an array at the given index.
+     * Complexity is O(n) under worst case.
      */
     void deleteElement(int index)
     {
-        
+        int deletedValue = 0;
+        if(index < 0 && index > this->getSize())
+        {
+            printf("indvalid index...");
+            return ;
+        }
+
+        for(int i = index; i < this->getLength(); i++)
+        {
+            deletedValue = A[index];
+            A[i] = A[i+1];
+        }
+        A[length-1] = 0;
+        this->length--;
     }
 };
 
@@ -73,8 +105,16 @@ int main(int argc, char const *argv[])
     int arrTemp[5] = {1, 2, 3, 4, 5};
     arr->initializeArray(arrTemp);
     arr->display();
-    printf("%d", arr->getSize());
-    printf("%d", arr->getLength());
+    printf("\n%d", arr->getSize());
+    printf("\n%d", arr->getLength());
+
+    arr->insertElement(6,3);
+    arr->display();
+    printf("\n%d", arr->getLength());
+
+    arr->deleteElement(0);
+    arr->display();
+    printf("\n%d", arr->getLength());
 
     return 0;
 }
