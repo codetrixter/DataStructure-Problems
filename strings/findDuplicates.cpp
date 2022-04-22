0bde61f4f63cf7c5f892dab3429409d421954d4a
codetrixter <Abhishek20893@gmail.com>
2022-04-22 21:12:07 +0530

    strings manipulation

---
+++ strings/findDuplicates.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file findDuplicates.cpp
+ * @author Abhishek
+ * @brief Finding the duplicates in the string, two ways:
+ * 1- Brute force(O(n^2))
+ * 2- hashing method(O(n))-> implemented here.
+ * @version 0.1
+ * @date 2022-04-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
